69ff3058011671ce95c0b17fd5735c286123ed60
Bülent Ala <bulentala@gmail.com>
2023-10-07 18:58:51 +0300

    ok

--- app/api/route.js
+++ app/api/route.js
@@ -1,100 +1,44 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
-  try {
-    // Simulate fetching data from an external source
-    const data = {
-      kind: "youtube#videoListResponse",
-      etag: "3_e1kOhHqHKuEb7Vt53BMMKyCxY",
-      items: [
-        {
-          kind: "youtube#video",
-          etag: "Bj9EMxmXZWwknaJ6SiirV40KcAU",
-          id: "lL9ip-73wtY",
-          snippet: {
-            publishedAt: "2023-10-07T05:00:18Z",
-            channelId: "UCdS7OE5qbJQc7AG4SwlTzKg",
-            title:
-              "Dilan Polat ve dahası? Avukat Feyza Altun, Fatih Altaylı'nın sorularını yanıtladı",
-            description:
-              'Teke Tek Bilim ▷ https://www.youtube.com/@TekeTekBilim \n\n00:00 Giriş\n03:59 Dilan Polat nasıl gündeme geldi?\n06:33 Mail Order nedir?\n09:02 Bu değirmenin suyu nereden geliyor?\n14:46 Polat çiftinin fabrikaları nerede?\n17:24 Merdiven altı kozmetik ürünü üretimleri?\n22:38 Feyza Altun, Dilan Polat ile tanışıyor mu?\n23:45 O sırada Sağlık Bakanlığı?\n25:01 Kozmetik mafyaları?\n27:49 Çiftin gelirinin vergilendirildiği nasıl araştırılabilir?\n33:50 Cihaz üretimi yapıyorlar mı?\n36:52 Rakiplerinden şikayet var mı?\n38:08 "Bu bir halk sağlığı sorunudur"\n40:15 Bu ürünler takip edilebilir mi?\n43:58 Bu davanın sonucu ne olur?\n47:45 Ticari zeka ve vergi mükellefliği?\n52:55 Hızlı Boşanma nedir?\n1:02:41 Kadın hakları ve İstanbul Sözleşmesi?\n1:06:39 Kapanış\n\nYouTube kanalına abone olmak için ▷ http://bit.ly/FatihAltayli\n\nGazeteci - Yazar Fatih Altaylı, Youtube kanalına özel gündemi yorumluyor.',
-            thumbnails: {
-              default: {
-                url: "https://i.ytimg.com/vi/lL9ip-73wtY/default.jpg",
-                width: 120,
-                height: 90,
-              },
-              medium: {
-                url: "https://i.ytimg.com/vi/lL9ip-73wtY/mqdefault.jpg",
-                width: 320,
-                height: 180,
-              },
-              high: {
-                url: "https://i.ytimg.com/vi/lL9ip-73wtY/hqdefault.jpg",
-                width: 480,
-                height: 360,
-              },
-              standard: {
-                url: "https://i.ytimg.com/vi/lL9ip-73wtY/sddefault.jpg",
-                width: 640,
-                height: 480,
-              },
-              maxres: {
-                url: "https://i.ytimg.com/vi/lL9ip-73wtY/maxresdefault.jpg",
-                width: 1280,
-                height: 720,
-              },
-            },
-            channelTitle: "Fatih Altaylı",
-            tags: [
-              "fatih altaylı kanalı",
-              "teke tek",
-              "teketek",
-              "teke tek özel",
-              "fatih altaylı youtube kanalı",
-              "fatih altaylı köşe yazıları",
-              "fatih altaylı",
-              "fatih altaylı video",
-              "fatih altaylı youtube",
-              "fatih altaylı yorum",
-              "dilan polat",
-              "dilan polat vergi",
-              "dilan polat engin polat",
-              "engin polat",
-              "dilan polat instagram",
-              "fatih altaylı feyza altun",
-              "feyza altun dilan polat",
-              "dilan polat kardeşi",
-              "sıla doğu",
-              "sıla doğu dilan polat",
-              "dilan polat fatih altaylı",
-            ],
-            categoryId: "22",
-            liveBroadcastContent: "none",
-            localized: {
-              title:
-                "Dilan Polat ve dahası? Avukat Feyza Altun, Fatih Altaylı'nın sorularını yanıtladı",
-              description:
-                'Teke Tek Bilim ▷ https://www.youtube.com/@TekeTekBilim \n\n00:00 Giriş\n03:59 Dilan Polat nasıl gündeme geldi?\n06:33 Mail Order nedir?\n09:02 Bu değirmenin suyu nereden geliyor?\n14:46 Polat çiftinin fabrikaları nerede?\n17:24 Merdiven altı kozmetik ürünü üretimleri?\n22:38 Feyza Altun, Dilan Polat ile tanışıyor mu?\n23:45 O sırada Sağlık Bakanlığı?\n25:01 Kozmetik mafyaları?\n27:49 Çiftin gelirinin vergilendirildiği nasıl araştırılabilir?\n33:50 Cihaz üretimi yapıyorlar mı?\n36:52 Rakiplerinden şikayet var mı?\n38:08 "Bu bir halk sağlığı sorunudur"\n40:15 Bu ürünler takip edilebilir mi?\n43:58 Bu davanın sonucu ne olur?\n47:45 Ticari zeka ve vergi mükellefliği?\n52:55 Hızlı Boşanma nedir?\n1:02:41 Kadın hakları ve İstanbul Sözleşmesi?\n1:06:39 Kapanış\n\nYouTube kanalına abone olmak için ▷ http://bit.ly/FatihAltayli\n\nGazeteci - Yazar Fatih Altaylı, Youtube kanalına özel gündemi yorumluyor.',
-            },
-            defaultAudioLanguage: "tr",
-          },
-        },
-      ],
-      pageInfo: {
-        totalResults: 1,
-        resultsPerPage: 1,
-      },
-    };
+  return NextResponse.json({
+    kind: "youtube#video",
 
-    // Return a JSON response with the fetched data
-    return NextResponse.json(data);
-  } catch (error) {
-    // Handle errors and return an appropriate response
-    console.error("Error fetching data:", error);
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
-  }
+    id: "PjYWpd7xkaM",
+    snippet: {
+      publishedAt: "2023-07-31T03:59:46Z",
+      title: "DENEME",
+      description:
+        "Discord for any problems/errors/bugs: https://www.codewithantonio.com/discord\nGithub & Live Website: https://www.codewithantonio.com/projects/ai-companion\nClerk Authentication: https://bit.ly/3Kmyytc\nHotpot for AI Character Images: https://hotpot.ai/\nInspiration for the project: https://github.com/a16z-infra/companion-app\n\nIn this comprehensive tutorial, we'll explore the intricate details of building an advanced SaaS AI Companion using Next.js 13. Our AI Companion taps into the power of embeddings and the Pinecone vector database to ensure long-term memory retention, supplemented by the fast caching abilities of the Upstash Redis database. We will also use MySQL and Prisma for storing companions.\n\nHarnessing the revolutionary features of Next.js 13 App Router, our platform will facilitate swift client-side routing, making the user experience seamless and intuitive. Alongside, the integration of Stripe promises a hassle-free subscription system, while Clerk handles user authentication with finesse.\n\nThroughout this tutorial, you'll be exposed to a rich array of web development technologies, including React, Tailwind, and Prisma. Every step is meticulously explained, ensuring even those new to the realm of SaaS development can follow along with ease.\n\nWhether you're an aspiring developer or a seasoned expert, this tutorial holds gems of knowledge that can benefit everyone. Keen to create a state-of-the-art SaaS AI Companion? Grab your developer toolkit, maybe a cup of your favorite brew, and let's redefine the AI-empowered web universe of 2023 with Next.js 13!\n\nTimestamps\n00:00 Intro\n06:07 Environment Setup\n10:31 Folder Setup\n18:07 Clerk Authentication\n31:51 Navbar\n51:43 Sidebar\n01:03:46 Search Filter\n01:16:14 Category Filter\n01:37:12 Companion Creation Form\n02:33:41 Companion Creation API\n02:51:48 Companion List\n03:11:42 Chat Header\n03:40:55 Chat UI\n04:18:38 Memory Service\n04:45:00 Chat API\n05:15:35 Stripe API\n05:49:26 Stripe UI\n06:23:05 Deployment",
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/PjYWpd7xkaM/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/PjYWpd7xkaM/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/PjYWpd7xkaM/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+        standard: {
+          url: "https://i.ytimg.com/vi/PjYWpd7xkaM/sddefault.jpg",
+          width: 640,
+          height: 480,
+        },
+        maxres: {
+          url: "https://i.ytimg.com/vi/PjYWpd7xkaM/maxresdefault.jpg",
+          width: 1280,
+          height: 720,
+        },
+      },
+      channelTitle: "Code With Antonio",
+      defaultAudioLanguage: "en",
+    },
+  });
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,108 @@
-const HomePage = () => {
-  return <div>DENEME</div>;
+"use client";
+// pages/youtube.js
+import React, { useState, useEffect } from "react";
+
+const Youtube = () => {
+  const [videos, setVideos] = useState([]);
+  const [apiData, setApiData] = useState(null);
+
+  useEffect(() => {
+    const fetchYoutubeData = async () => {
+      try {
+        // Replace 'YOUR_API_KEY' with your actual YouTube Data API key
+        const apiKey = "AIzaSyCNjPUyB7TwEQNFkDxdV-fM-OXzg1LwkSM";
+        const playlistId = "PLBPVr0JQZNobnoYWApfoEi2qbuY3QRwTU";
+
+        // Fetch videos from the YouTube Data API
+        const playlistResponse = await fetch(
+          `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&key=${apiKey}`
+        );
+
+        // Check if the request was successful
+        if (playlistResponse.ok) {
+          // Parse the response as JSON
+          const playlistData = await playlistResponse.json();
+
+          // Extract video items from the playlist response
+          const videoItems = playlistData.items;
+
+          // Create an array to store details of each video
+          const videoDetails = [];
+
+          // Fetch details for each video using its ID
+          for (const video of videoItems) {
+            const videoId = video.snippet.resourceId.videoId;
+            const videoDetailResponse = await fetch(
+              `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${apiKey}`
+            );
+
+            if (videoDetailResponse.ok) {
+              const videoDetailData = await videoDetailResponse.json();
+              videoDetails.push(videoDetailData.items[0]);
+            } else {
+              console.error(
+                "Error fetching YouTube video details:",
+                videoDetailResponse.status,
+                videoDetailResponse.statusText
+              );
+            }
+          }
+
+          // Set the videos state with the details of each video
+          setVideos(videoDetails);
+        } else {
+          console.error(
+            "Error fetching YouTube playlist data:",
+            playlistResponse.status,
+            playlistResponse.statusText
+          );
+        }
+      } catch (error) {
+        console.error("Error fetching YouTube data:", error);
+      }
+    };
+
+    // Call the fetch function
+    fetchYoutubeData();
+  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api");
+        const data = await response.json();
+        setApiData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  const allData = apiData &&
+    videos && [
+      {
+        kind: apiData.kind,
+        title: apiData.snippet.title,
+        publishedAt: apiData.snippet.publishedAt,
+        description: apiData.snippet.description,
+        channelTitle: apiData.snippet.channelTitle,
+      },
+      ...videos.map((video) => ({
+        kind: apiData.kind,
+        title: video.snippet.title,
+        publishedAt: video.snippet.publishedAt,
+        description: video.snippet.description,
+        channelTitle: video.snippet.channelTitle,
+      })),
+    ];
+
+  return (
+    <div>
+      {allData.map((data, i) => {
+        return <div key={i}>{data.title}</div>;
+      })}
+    </div>
+  );
 };
 
-export default HomePage;
+export default Youtube;
