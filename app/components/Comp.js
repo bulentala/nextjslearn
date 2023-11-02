f1c585f0163af143b292e159c1db4e206a03fab6
Bülent Ala <bulentala@gmail.com>
2023-11-02 16:12:20 +0300

    edit

--- app/components/Comp.js
+++ app/components/Comp.js
@@ -1,44 +1,47 @@
 import Image from "next/image";
 import Link from "next/link";
-async function getYoutubePlayListData() {
+async function fetchYoutubePlaylistData() {
   const apiKey = "AIzaSyD8tAgjH9mE1injUVe0BzhZ-EsgiuGwYoQ";
   const playlistId = "PLBPVr0JQZNobnoYWApfoEi2qbuY3QRwTU";
-  const res = await fetch(
-    `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${playlistId}&key=${apiKey}`
-  );
-  if (!res.ok) {
+  const maxResults = "50";
+  const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=${maxResults}&playlistId=${playlistId}&key=${apiKey}`;
+  const response = await fetch(url);
+  if (!response.ok) {
     throw new Error("Failed to fetch data");
   }
-  return res.json();
+  return response.json();
 }
 const Comp = async () => {
-  const data = await getYoutubePlayListData();
+  const data = await fetchYoutubePlaylistData();
   return (
     <>
-      {data.items.map((item) => {
-        try {
-          return (
-            <Link href=''>
-              <div className='grid grid-cols-[auto_1fr] gap-3 mb-3 '>
-                <Image
-                  alt='test'
-                  src={item.snippet.thumbnails.default.url}
-                  width='120'
-                  height='90'
-                  className='rounded '
-                />
-                <div className=''>
-                  <div className=''>{item.snippet.title}</div>
-                  <div className=''>{item.snippet.videoOwnerChannelTitle}</div>
-                </div>
+      {data.items.map((video) => {
+        const {
+          title,
+          videoOwnerChannelTitle,
+          thumbnails: {
+            default: { url },
+          },
+        } = video.snippet;
+
+        return (
+          <Link href='' key={video.id}>
+            <div className='grid grid-cols-[auto_1fr] gap-3 mb-3'>
+              <Image
+                alt={title}
+                src={url}
+                width={120}
+                height={90}
+                className='rounded'
+              />
+              <div>
+                <div>{title}</div>
+                <div>{videoOwnerChannelTitle}</div>
               </div>
-            </Link>
-          );
-        } catch (error) {
-          return null;
-        }
+            </div>
+          </Link>
+        );
       })}
-      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
     </>
   );
 };
