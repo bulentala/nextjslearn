6740b3daefedde53b4d8c9ca07b553b41fe315eb
Bülent ALA <46373855+bulentala@users.noreply.github.com>
2023-12-17 20:59:11 +0300

    vercel analysrics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
@@ -19,7 +20,9 @@ export default function RootLayout({
     <html lang='en' className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body>
         <Header />
-        <main>{children}</main>
+        <main>
+          {children} <Analytics />
+        </main>
         <Footer />
       </body>
     </html>
